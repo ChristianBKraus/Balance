c28397bd811891af57c361f90e897242bd6301cb
Kraus Christian <christian.kraus@sap.com>
2018-10-09 11:24:12 +0000

    CDS Select and Message Handling

--- srv/src/main/java/jupiterpa/balance/Validation.java
+++ srv/src/main/java/jupiterpa/balance/Validation.java
@@ -23,15 +23,15 @@ public class Validation {
 	
 	public void validate(Transaction transaction) throws ValidationException {
 		if ( transaction.getAccount() == null ) {
-			throw new ValidationException("Account of Transaction not specified");
+			throw new ValidationException("TRANSACTION_ACCOUNT_INITIAL");
 		}
 		 if ( repo.getAccountById(transaction.getAccount_ID()) == null ) {
-		 	throw new ValidationException("Account with ID does not exist");			
+		 	throw new ValidationException("TRANSACTION_ACCOUNT_NOT_EXISTS",transaction.getAccount_ID().toString());			
 		 }
 		 if (transaction.getAmount() < 0.0) {
 		 	Balance balance = repo.getBalanceByAccountId(transaction.getAccount_ID());
 		 	if ( Math.abs( transaction.getAmount() ) > balance.getAmount() ) {
-		 		throw new ValidationException("Not enough balance remaining");				
+		 		throw new ValidationException("TRANSACTION_BALANCE_LOW",transaction.getAccount_ID().toString(),balance.getAmount().toString());				
 		 	}
 		 }
 	}
